0f30681a4317df2e6e0d401bac7da03716fa672c
Eric Lin <eric.lin@washpost.com>
2016-01-19 12:35:20 -0500

    Added autoplay video on pulse mobile. Supporting video on pulse mobile.

--- pulse-mobile-300x250/js/scripts.js
+++ pulse-mobile-300x250/js/scripts.js
@@ -30,7 +30,6 @@
             var slide_count = _slick.slideCount - 1;
 
             $pulse.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-
                 if (slide_count === nextSlide) {
                     _$('.js-pulse-animate').slideUp();
                 }
@@ -38,38 +37,57 @@
                 if (!_$('.js-pulse-animate').is(':visible')) {
                     _$('.js-pulse-animate').slideDown();
                 }
-
             });
-        };
+        }
 
         function loadCarousel() {
             $pulse.slick({
                 lazyLoad: 'ondemand',
                 rtl: false
             });
-        };
+        }
 
         function loadSlick() {
 
             //use parent jquery scope
             return $.ajax({
-                url: 'https://js.washingtonpost.com/wp-stat/advertising/WellsFargo/WF_300x250_Mobile/js/slick.min.js',
+                url: 'https://www.washingtonpost.com/wp-stat/advertising/PostPulse/js/mobile-300x250/slick.min.js',
                 dataType: "script",
                 cache: true,
                 success: function(results) {}
             });
 
-        };
+        }
+
+        function autoPlayVideoOnSlide(){
+            var $pulseVideo = $pulse.find('.pulse-video').not(".slick-cloned");
+            if(!$pulseVideo.length){
+                return;
+            }
+            var videoSlideIndex = $pulseVideo.data('slick-index'),
+            pulseVideo = $pulseVideo.find('#pulse-player').get(0);
+            $pulse.on('afterChange', function(event, slick, currentSlide){
+                if(currentSlide === videoSlideIndex){
+                    pulseVideo.play();
+                }else{
+                    pulseVideo.pause();
+                }
+            });
+            $(pulseVideo).on('ended', function(){
+                pulseVideo.play();
+            });
+        }
 
         function init() {
 
             loadSlick().done(function(data, textStatus, jqXHR) {
                 loadCarousel();
                 headerFooterSlide();
+                autoPlayVideoOnSlide();
                 //PulseTracking.init();
             });
 
-        };
+        }
 
         return {
             init: init
