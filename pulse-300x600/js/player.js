8b8ebeb744af3906c6ec1f4da348df6bb8531439
Lemieux, Matthew <Matthew.Lemieux@washpost.com>
2016-01-25 13:16:12 -0500

    AD-16528 Add video tracking

--- pulse-300x600/js/player.js
+++ pulse-300x600/js/player.js
@@ -1,81 +1,90 @@
 (function($) {
 
-        var PulsePlayer = (function() {
+    var _$ = $.proxy($.fn.find, $(document));
 
-            var _$ = $.proxy($.fn.find, $(document));
+    var PulsePlayer = (function() {
 
-            var video = document.getElementById('pulse-player'),
-                click_thru = _$('.pulse-wrapper').data('click'),
-                $video_play_button = _$('.pulse-player-play'),
-                $video_mute_button = _$('.pulse-player-volume');
-                $has_video_ctrls   = _$('.pulse-player-no-ctrls').length > 0 ? false : true;
+        var video = document.getElementById('pulse-player'),
+            click_thru = _$('.pulse-wrapper').data('click'),
+            tracking_url = _$('.pulse-wrapper').data('track') ? _$('.pulse-wrapper').data('track') : false;
+            $video_play_button = _$('.pulse-player-play'),
+            $video_mute_button = _$('.pulse-player-volume');
+            $has_video_ctrls = _$('.pulse-player-no-ctrls').length > 0 ? false : true;
 
-
-            function advClickThru(event) {
-                event.preventDefault();
-                top.window.open(click_thru);
+        function playBtnTrack(tracking_url) {
+            if (tracking_url) {
+                var rand_num = Math.floor(Math.random() * 10000000) + '?',
+                    tracking_url = tracking_url + rand_num;
+                _$('.pulse-tracking-wrapper').append('<img src="' + tracking_url + '" alt="" border="0" height="1" width="1" style="display:none;" />');
             }
+        }
 
-            function muteBtnClick(event) {
-                event.preventDefault();
-                if (video.muted) {
-                    video.muted = false;
-                    _$(this).removeClass('fa-volume-off');
-                } else {
-                    video.muted = true;
-                    _$(this).addClass('fa-volume-off');
+        function advClickThru(event) {
+            event.preventDefault();
+            top.window.open(click_thru);
+        }
 
-                }
+        function muteBtnClick(event) {
+            event.preventDefault();
+            if (video.muted) {
+                video.muted = false;
+                _$(this).removeClass('fa-volume-off');
+            } else {
+                video.muted = true;
+                _$(this).addClass('fa-volume-off');
+
             }
+        }
 
-            function playBtnClick(event) {
-                event.preventDefault();
-                if (video.paused || video.ended) {
-                    video.play();
-                    _$(video).parent().addClass('pulse-player-active');
-                    _$(video).css('visibility', 'visible');
-                    _$(this).addClass('fa-pause-circle-o pulse-player-pause');
-                } else {
-                    video.pause();
-                    _$(this).removeClass('fa-pause-circle-o pulse-player-pause');
-                }
+        function playBtnClick(event) {
+            event.preventDefault();
+            if (video.paused || video.ended) {
+                playBtnTrack(tracking_url);
+                video.play();
+                _$(video).parent().addClass('pulse-player-active');
+                _$(video).css('visibility', 'visible');
+                _$(this).addClass('fa-pause-circle-o pulse-player-pause');
+            } else {
+                video.pause();
+                _$(this).removeClass('fa-pause-circle-o pulse-player-pause');
             }
+        }
 
-            function videoEndEvents() {
-                video.load();
-                if($has_video_ctrls){
-                    $video_play_button.removeClass('fa-pause-circle-o pulse-player-pause').show();
-                }
+        function videoEndEvents() {
+            video.load();
+            if ($has_video_ctrls) {
+                $video_play_button.removeClass('fa-pause-circle-o pulse-player-pause').show();
             }
+        }
 
-            function videoBtnEffects() {
-                _$(".pulse-wrapper").on("mouseenter", ".pulse-player-active", function() {
-                    $video_play_button.fadeIn();
-                });
-                _$(".pulse-wrapper").on("mouseleave", ".pulse-player-active", function() {
-                    $video_play_button.fadeOut();
-                });
-            }
+        function videoBtnEffects() {
+            _$(".pulse-wrapper").on("mouseenter", ".pulse-player-active", function() {
+                $video_play_button.fadeIn();
+            });
+            _$(".pulse-wrapper").on("mouseleave", ".pulse-player-active", function() {
+                $video_play_button.fadeOut();
+            });
+        }
 
-            function bindFunctions() {
-                _$(video).on('click', advClickThru);
-                _$('.pulse-player-poster').on('click', advClickThru);
-                $video_mute_button.on('click', muteBtnClick);
-                $video_play_button.on('click', playBtnClick);
-                _$(video).on('ended', videoEndEvents);
-            }
+        function bindFunctions() {
+            _$(video).on('click', advClickThru);
+            _$('.pulse-player-poster').on('click', advClickThru);
+            $video_mute_button.on('click', muteBtnClick);
+            $video_play_button.on('click', playBtnClick);
+            _$(video).on('ended', videoEndEvents);
+        }
 
-            function init() {
-                bindFunctions();
-                videoBtnEffects();
-            }
+        function init() {
+            bindFunctions();
+            videoBtnEffects();
+        }
 
-            return {
-                init: init
-            };
+        return {
+            init: init
+        };
 
-        })();
+    })();
 
-        PulsePlayer.init();
+    PulsePlayer.init();
 
-    })(parent.window.jQuery);
+})(parent.window.jQuery);
